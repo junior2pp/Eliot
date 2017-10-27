1b239ba839d880d9b27a38aa068e09a51d57918f
junior2pp <junior2pp@hotmail.com>
2017-10-27 16:54:06 -0500

    struct neurona con los campos de tablaAnd, tablaOr y pesos

--- generarTablas.go
+++ generarTablas.go
@@ -7,39 +7,36 @@ import (
 
 func main() {
 
-	var t Tabla
-	t.crearAnd(3)
+	var n1 Neurona
+	n1.crearAnd(3)
 	alternador(4,8)
 }
 
-type Tabla struct {
-	and [][]int
-	or  [][]int
-}
-
-type Pesos struct {
-	pesos [][]float32
+type Neurona struct {
+	tablaAnd [][]int
+	tablaOr  [][]int
+	pesos []float32
 }
 
 func calcularPotencia(valor float64) float64  {
 	return math.Pow(2,valor)
 }
 
-func (t *Tabla) crearAnd(variables int)  {
+func (t *Neurona) crearAnd(variables int)  {
 	filas := int(calcularPotencia(float64(variables)))
-	t.and = make([][]int,variables+1)
-	for x := 0; x < len(t.and) ; x++  {
-		t.and[x] = make([]int,filas)
+	t.tablaAnd = make([][]int,variables+1)
+	for x := 0; x < len(t.tablaAnd) ; x++  {
+		t.tablaAnd[x] = make([]int,filas)
 		for j := 0; j < filas ; j++  {
-			t.and[x][j] = 1
+			t.tablaAnd[x][j] = 1
 		}
 	}
-	fmt.Println(t.and)
+	fmt.Println(t.tablaAnd)
 
 
 }
 
-func (t Tabla) crearOr(variables int)  {
+func (t *Neurona) crearOr(variables int)  {
 
 }
 
@@ -51,6 +48,7 @@ cantidad: 2  y longitud 8
 [0 0 1 1 0 0 1 1]
 
 */
-func alternador(cantidad, longitud int) (array []int)  {
 
+func alternador(cantidad, longitud int) (array []int)  {
+	return nil
 }
